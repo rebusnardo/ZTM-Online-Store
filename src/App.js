6966587e9accf3f6ee42713e423e53edf10630be
rebusnardo <ron.busnardo@gmail.com>
2022-12-24 08:10:03 -0800

    added sass

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import "./categories.styles.scss";
+
 function App() {
   const categories = [
     {
@@ -24,8 +26,8 @@ function App() {
 
   return (
     <div className="categories-container">
-      {categories.map(({ title }) => (
-        <div className="category-container">
+      {categories.map(({ title, id }) => (
+        <div className="category-container" key={id}>
           <div className="background-image" />
           <div className="category-body-container">
             <h2>{title}</h2>
