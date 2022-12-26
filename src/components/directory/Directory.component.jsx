a51d243e1ff95219e3894c25f3515143f589c46c
rebusnardo <ron.busnardo@gmail.com>
2022-12-25 17:09:13 -0800

    updating our directory page

---
+++ src/components/directory/Directory.component.jsx
@@ -0,0 +1,15 @@
+import "./directory.styles.scss";
+import CategoryItem from "../category-item/category-item.component";
+
+const Directory = ({ categories }) => {
+  console.log(categories);
+  return (
+    <div className="directory-container">
+      {categories.map((category) => (
+        <CategoryItem category={category} key={category.id} />
+      ))}
+    </div>
+  );
+};
+
+export default Directory;
