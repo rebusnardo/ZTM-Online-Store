0b5212fc19e467fc90585ba5853a849ca9e44b82
rebusnardo <ron.busnardo@gmail.com>
2023-01-10 06:37:28 -0800

    finished sign-in

--- src/components/cart-icon/Cart-icon.component.jsx
+++ src/components/cart-icon/Cart-icon.component.jsx
@@ -1,7 +1,6 @@
-import { ReactComponent as ShoppingIcon } from "../../assets/shopping-bag.svg";
 import { useContext } from "react";
 import { CartContext } from "../../contexts/Cart.context";
-import "./cart-icon.styles.scss";
+import { ShoppingIcon, CartIconContainer, ItemCount } from "./cart-icon.styles";
 
 import React from "react";
 
@@ -11,10 +10,10 @@ const Cart = () => {
   const toggleIsCartOpen = () => setIsCartOpen(!isCartOpen);
 
   return (
-    <div className="cart-icon-container" onClick={toggleIsCartOpen}>
+    <CartIconContainer onClick={toggleIsCartOpen}>
       <ShoppingIcon className="shopping-icon" />
-      <span className="item-count">{cartCount}</span>
-    </div>
+      <ItemCount>{cartCount}</ItemCount>
+    </CartIconContainer>
   );
 };
 
--- src/components/directory-item/Directory-item.component.jsx
+++ src/components/directory-item/Directory-item.component.jsx
@@ -1,18 +1,19 @@
-import "./directory-item.styles.scss";
+import {
+  BackgroundImage,
+  Body,
+  DirectoryItemConstainer,
+} from "./directory-item.styles";
 
 const DirectoryItem = ({ category }) => {
   const { imageUrl, title } = category;
   return (
-    <div className="directory-item-container">
-      <div
-        className="background-image"
-        style={{ backgroundImage: `url(${imageUrl})` }}
-      />
-      <div className="body">
+    <DirectoryItemConstainer>
+      <BackgroundImage imageUrl={imageUrl} />
+      <Body>
         <h2>{title}</h2>
         <p>Shop Now</p>
-      </div>
-    </div>
+      </Body>
+    </DirectoryItemConstainer>
   );
 };
 
