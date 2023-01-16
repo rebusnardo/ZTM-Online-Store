0dfaabc98f24a0864b739adf17b5a6515a229273
rebusnardo <ron.busnardo@gmail.com>
2023-01-16 06:58:47 -0800

    latest

--- src/contexts/User.context.js
+++ src/contexts/User.context.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { useState, useEffect } from "react";
+import { useEffect, useReducer } from "react";
 import { createContext } from "react";
 
 import {
@@ -13,8 +13,37 @@ export const UserContext = createContext({
   setCurrentUser: () => null,
 });
 
+export const USER_ATCION_TYPES = {
+  SET_CURRENT_USER: "SET_CURRENT_USER",
+};
+
+const userReducer = (state, action) => {
+  console.log("dispatched");
+  console.log(action);
+  const { type, payload } = action;
+
+  switch (type) {
+    case USER_ATCION_TYPES.SET_CURRENT_USER:
+      return { ...state, currentUser: payload };
+
+    default:
+      throw new Error(`unHandles type ${type} in userReducer`);
+  }
+};
+
+const INITIAL_STATE = {
+  currentUser: null,
+};
+
 export const UserProvider = ({ children }) => {
-  const [currentUser, setCurrentUser] = useState(null);
+  // const [currentUser, setCurrentUser] = useState(null);
+  const [{ currentUser }, dispatch] = useReducer(userReducer, INITIAL_STATE);
+  console.log(currentUser);
+
+  const setCurrentUser = (user) => {
+    dispatch({ type: USER_ATCION_TYPES.SET_CURRENT_USER, payload: user });
+  };
+
   const value = { currentUser, setCurrentUser };
 
   useEffect(() => {
