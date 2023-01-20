72d117b799d04439c699b3d51b69ab7a6ddff7ef
rebusnardo <ron.busnardo@gmail.com>
2023-01-20 17:59:35 -0500

    latest commit 20jan2023

--- src/routes/shop/Shop.component.jsx
+++ src/routes/shop/Shop.component.jsx
@@ -1,12 +1,26 @@
 import { Routes, Route } from "react-router-dom";
 // import { CategoriesProvider } from "../../contexts/Categories.context";
-
+// import { useEffect } from "react";
+// import { useDispatch } from "react-redux";
+// import { setCategoriesMap } from "../../store/categories/category.action";
+// import { getCategoriesAndDocuments } from "../../utils/firebase/Firebase.utils";
 import CategoriesPreview from "../categories-preview/categories-preview.component";
 import Category from "../category/Category.component";
 
 import "./shop.styles.scss";
 
 const Shop = () => {
+  // const dispatch = useDispatch();
+
+  // useEffect(() => {
+  //   const getCategoriesMap = async () => {
+  //     const categoryMap = await getCategoriesAndDocuments("categories");
+  //     dispatch(setCategoriesMap(categoryMap));
+  //   };
+
+  //   getCategoriesMap();
+  // }, []);
+
   return (
     <Routes>
       <Route index element={<CategoriesPreview />} />
